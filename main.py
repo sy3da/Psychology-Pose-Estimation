55872bdf25fa4e79196650a1d8212e239181b258
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-07-12 18:28:25 -0400

    start structuring code

---
+++ main.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    print("Running main.py...")
+
+    # Get camera data
+
+    # Convert camera distance units to meters
+    
+    # Get list of pixel coords for each landmark (use MediaPipe)
+
+    # Append depth data to each pixel coord
+
+    # Output data as csv file
+    
