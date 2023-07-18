2a31f518bd83ff8df249e3f954f8767d9db5e2c6
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-07-18 16:08:38 -0400

    relocate test clips, make filepath work regardless of the directory from which the script is ran

--- src/pose_module.py
+++ src/pose_module.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 import numpy as np
+import os
 
 
 class PoseDetector():
@@ -13,7 +14,10 @@ class PoseDetector():
 
 
 def main():
-    input_video = "Data/test_videos/1_walking_toward.mp4"
+    # Get the directory where the running script is located
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    input_video = os.path.join(script_directory, "test_videos", "1_walking_toward.mp4")
+    # input_video = "../Data/test_videos/1_walking_toward.mp4"
     video = cv2.VideoCapture(input_video)
     if not video.isOpened():
         print("Error opening video file:", input_video)
