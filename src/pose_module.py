321c22568499d89aa15d8c9d787761e7e938a483
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-07-18 21:49:56 -0400

    implement guard against invalid landmarks

--- src/pose_module.py
+++ src/pose_module.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 import time
 import numpy as np
 import os
+from typing import Tuple
+import math
 
 
 class PoseLandmarker():
@@ -38,7 +40,7 @@ class PoseLandmarker():
 
         return
 
-    def get_landmarks(self, image, draw=False):
+    def get_landmarks(self, image: np.ndarray, draw: bool = False) -> Tuple[bool, bool, np.ndarray]:
         """
         Detect pose and return pose landmarks for the given image.
 
@@ -47,8 +49,11 @@ class PoseLandmarker():
             draw (bool): Whether to draw the pose skeleton on the image.
 
         Returns:
-            tuple[bool, np.ndarray]: A tuple containing:
+            tuple[bool, bool, np.ndarray]: A tuple containing:
                 - pose_detected (bool): Indicates whether a pose was detected in the image.
+                - contains_invalid_landmarks (bool): Indicates whether the returned landmarks_pixels array contains
+                    invalid landmarks (i.e. landmarks that are outside of the image bounds or landmarks located at
+                    negative-valued normalized pixel coordinates).
                 - landmarks_pixels (np.ndarray): An array of shape (33, 2) representing the pixel coordinates (x, y)
                   for each of the 33 total pose landmarks detected. The i-th row corresponds to the i-th landmark
                   (zero-indexed, so row 0 is landmark 1).
@@ -73,19 +78,87 @@ class PoseLandmarker():
         landmarks_pixels = np.zeros((33, 2), dtype="int")
 
         pose_detected = False
+        contains_invalid_landmarks = False
 
         if results.pose_landmarks:
+            # Pose detected
+            pose_detected = True
+
             if draw:
                 self.mp_draw.draw_landmarks(image, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS, self.drawing_spec_landmark,
                                                 self.drawing_spec_connection)
+            
+            # Loop through each landmark
+            for id, landmark in enumerate(results.pose_landmarks.landmark):
+                # There are 33 landmarks in total, each with x, y, z normalized coordinates and a visibility value
+                # print(id, landmark)
 
-        return pose_detected, landmarks_pixels
+                # Convert normalized coordinates to pixel coordinates (NOTE: z is currently unused)
+                # x, y = int(landmark.x * image_width), int(landmark.y * image_height)
+                x, y = self._normalized_to_pixel_coordinates(landmark.x, landmark.y, image_width, image_height)
+
+                if (x, y) == (-1, -1):
+                    # This landmark is invalid
+                    contains_invalid_landmarks = True
+
+                # print(id, x, y)
+
+                # Overlay blue dots on each landmark to verify pixel coordinates
+                # cv2.circle(image, (x, y), 5, (255, 0, 0), cv2.FILLED)
+
+                # Store pixel coordinates in array
+                landmarks_pixels[id] = (x, y)
+
+        return pose_detected, contains_invalid_landmarks, landmarks_pixels
+    
+    def _is_valid_normalized_value(self, normalized_value: float) -> bool:
+        """
+        Checks if a value is a valid normalized value in the range [0, 1].
+    
+        Args:
+            normalized_value (float): The value to be checked.
+        
+        Returns:
+            bool: True if the value is within the range [0, 1], False otherwise.
+        """
+        return (normalized_value > 0 or math.isclose(0, normalized_value)) and (normalized_value < 1 or math.isclose(1, normalized_value))
+    
+    def _normalized_to_pixel_coordinates(self, normalized_x: float, normalized_y: float,
+                                         image_width: int, image_height: int) -> Tuple[int, int]:
+        """
+        Converts normalized pixel coordinates to pixel coordinates in the image by scaling with image dimensions.
+
+        Args:
+            normalized_x (float): The x-coordinate in normalized units [0, 1].
+            normalized_y (float): The y-coordinate in normalized units [0, 1].
+            image_width (int): The width of the image in pixels.
+            image_height (int): The height of the image in pixels.
+
+        Returns:
+            Tuple[int, int]: A tuple containing the pixel coordinates (x, y).
+            
+            If the provided normalized coordinates are valid, the function returns the corresponding pixel coordinates
+            inside the image.
+            
+            If the provided normalized coordinates are invalid (negative or outside of the image bounds),
+            the function returns (-1, -1).
+        """
+
+        if not (self._is_valid_normalized_value(normalized_x) and self._is_valid_normalized_value(normalized_y)):
+            # At least one of the normalized coordinates is invalid
+            # TODO: Draw coordinates even if it's outside of the image bounds.
+            return -1, -1
+        
+        x_px = min(math.floor(int(normalized_x * image_width)), image_width - 1)
+        y_px = min(math.floor(int(normalized_y * image_height)), image_height - 1)
+        return x_px, y_px
 
 
 def main():
     # Get the directory where the running script is located
     script_directory = os.path.dirname(os.path.realpath(__file__))
     input_video = os.path.join(script_directory, "test_videos", "1_walking_toward.mp4")
+    # input_video = os.path.join(script_directory, "test_videos", "6_two_people_walking_towards.mp4")
     # input_video = "../Data/test_videos/1_walking_toward.mp4"
     video = cv2.VideoCapture(input_video)
     if not video.isOpened():
@@ -105,7 +178,7 @@ def main():
             break
 
         # Get pixel locations of all pose landmarks
-        pose_detected, landmarks_pixels = detector.get_landmarks(image=frame, draw=True)
+        pose_detected, contains_invalid_landmarks, landmarks_pixels = detector.get_landmarks(image=frame, draw=True)
 
         # if pose_detected:
         #     # Do something with the landmarks
