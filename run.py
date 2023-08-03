e8ec2ccc2a388876ba9fc2c49d6a9ba791e73249
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-08-03 18:34:18 -0400

    got camera video to load and skeleton applied. using adaptive brightness compensation

---
+++ run.py
@@ -0,0 +1,212 @@
+import os
+import sys
+import subprocess
+import shutil
+import argparse
+import time
+from typing import Optional
+
+from src.bin_to_csv import BinToCsv
+
+def record_skv() -> Optional[str]:
+    """
+    Launches Automotive Suite and waits until the user exits it.
+    Once the user is done recording, they can close the program.
+    After it is closed, moves all of the newly recorded files from the
+    Automotive Suite recordings directory to the './skvs/' directory.
+    
+    Returns:
+        str: The absolute path to the './skvs/' directory if new files were recorded,
+        otherwise script terminates.
+    
+    NOTE: If no new files were recorded, terminates the script.
+    """
+    # Find the folder path that matches the pattern "./automotive_suite_recorder_viewer*"
+    # print(os.getcwd())
+    folder_name = next(filter(lambda x: x.startswith('automotive_suite_recorder_viewer'), os.listdir()), None)
+    # print(f"folder_name: {folder_name}")
+    
+    if not folder_name:
+        print('Automotive Suite not found at automotive_suite_recorder_viewer*/automotive_suite.exe')
+        sys.exit()
+    else:
+        folder_path = os.path.join(os.getcwd(), folder_name)
+        exe_path = os.path.join(folder_path, 'automotive_suite.exe')
+        # print(f"folder_path: {folder_path}")
+        # print(f"exe_path: {exe_path}")
+    
+        # Get set of files in ./automotive_suite_recorder_viewer*
+        #   - Put all *.skv filenames and the datetime they were created in a set of tuples {("filename.skv", datetime_created))}
+        #   - Ignore folders
+
+        recordings_path = os.path.join(folder_path, 'RecordedMovies')
+
+        skvs_before_recording = set(filter(lambda x: x[0].endswith('.skv'), map(lambda x: (x, os.path.getctime(os.path.join(recordings_path, x))), os.listdir(recordings_path))))
+
+        # skvs_before_recording = set()
+        # for filename in os.listdir(recordings_path):
+        #     print(f"filename: {filename}")
+        #     if os.path.isfile(os.path.join(recordings_path, filename)) and filename.endswith('.skv'):
+        #         # get the datetime the file was created
+        #         datetime_created = os.path.getctime(os.path.join(recordings_path, filename))
+        #         # add the filename and datetime_created to the set as a tuple
+        #         skvs_before_recording.add((filename, datetime_created))
+
+
+
+
+        # print("skvs before recording")
+        # print(skvs_before_recording)
+    
+        # Launch the program and wait until the user exits it
+        process = subprocess.run(exe_path, shell=True)
+    
+        skvs_after_recording = set(filter(lambda x: x[0].endswith('.skv'), map(lambda x: (x, os.path.getctime(os.path.join(recordings_path, x))), os.listdir(recordings_path))))
+        # print("skvs after recording")
+        # print(skvs_after_recording)
+    
+        # If sets are different, then new .skv file(s) were created
+        # skvs_before_recording - skvs_after_recording = set of new .skv files
+        if skvs_before_recording != skvs_after_recording:
+            # Move the new .skv file(s) to ./skvs/
+            new_skvs = skvs_after_recording - skvs_before_recording
+            # print("new_skvs")
+    
+            # # Get absolute path to the new .skv file
+            # new_skv_path = os.path.join(recordings_path, new_skv[0])
+
+            for new_skv in new_skvs:
+                # print(new_skv)
+                new_skv_path = os.path.join(recordings_path, new_skv[0])
+                # print(new_skv_path)
+                shutil.move(new_skv_path, os.path.join(os.getcwd(), 'skvs'))
+                print('Automotive Suite recorded new file: ' + new_skv_path)
+            
+            skv_dir = os.path.join(os.getcwd(), 'skvs')
+    
+            return skv_dir
+        else:
+            print('No new .skv file was recorded')
+            sys.exit()
+
+def check_for_skvs(skvs_dir: str):
+    """
+    Checks if there are any .skv files in skvs_dir.
+    If there are no .skv files, terminates the script.
+
+    Args:
+        skvs_dir (str): absolute path to directory containing skv files
+
+    Returns:
+        None
+    """
+    skvs_before_recording = set(filter(lambda x: x[0].endswith('.skv'), map(lambda x: (x, os.path.getctime(os.path.join(skvs_dir, x))), os.listdir(skvs_dir))))
+
+    # if skvs_before_recording empty, exit
+    if not skvs_before_recording:
+        print('No .skv files found in: ' + skvs_dir)
+        print('Please record a .skv file using Automotive Suite.')
+        sys.exit()
+    
+    return
+
+def skv_to_bin(skvs_dir: str):
+    """
+    Convert each .skv file in skvs_dir to .bin file using imx520_sample.exe and save to bins_dir
+
+    Args:
+        skvs_dir (str): absolute path to directory containing skv files
+
+    Returns:
+        None
+    """
+
+    # Get absolute path of directory where .bin files will be saved to
+    bins_dir = os.path.join(skvs_dir, "bins")
+
+    # Convert all .skv video files in skvs_dir into .bin files using imx520_sample.exe and save to bins_dir
+
+    # Get absolute path to imx520_sample.exe
+    imx520_sample_exe_path = os.path.join(os.getcwd(), "src/skv_to_mat/r2_3/imx520_sample.exe")
+    
+    # Run imx520_sample.exe
+    # ./imx520_sample.exe -i ./skvs/ -o ./skvs/mat/ -d
+    process = subprocess.run([imx520_sample_exe_path, "-i", skvs_dir, "-o", bins_dir, "-d"], shell=True)
+
+    return
+
+def bin_to_csv(skvs_dir: str):
+    """
+    Take all .bin files in bins_dir, run through pose estimation pipeline, and save output to csvs_dir
+
+    Args:
+        skvs_dir (str): absolute path to directory containing skv files
+
+    Returns:
+        None
+    """
+
+    # Get absolute path of directory where .bin files are located
+    bins_dir = os.path.join(skvs_dir, "bins")
+
+    # Run pose estimation pipeline on all .bin files in bins_dir and save output to csvs_dir
+    myBinToCsv = BinToCsv(input_dir=bins_dir, output_filename="my_csv", visualize_Pose=True)
+    myBinToCsv.run()
+
+    return
+
+  
+def process_args() -> argparse.Namespace:
+    """
+    Process command line arguments
+
+    Returns:
+        args (argparse.Namespace): parsed command line arguments
+
+    Notes:
+        If no command-line arguments are provided, all options are set to True by default.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--skv_to_bin', action='store_true', help='Convert .skv files to .bin files')
+    parser.add_argument('--bin_to_csv', action='store_true', help='Take all .bin files and generate output .csv file')
+    args = parser.parse_args()
+
+    # If no args are provided, set all 3 bools to True
+    if not any(vars(args).values()):
+        args.skv_to_bin = True
+        args.bin_to_csv = True
+    
+    return args
+
+if __name__ == '__main__':
+    # TODO: Add checks to ensure that skvs_dir and bins_dir exist. If not, create them.
+    
+    main_start_time = time.time()
+    
+    args = process_args()
+
+    # Get the path to the new .skv file
+    # skvs_dir = record_skv()
+
+    skvs_dir = os.path.join(os.getcwd(), 'Data', 'skvs')
+
+    check_for_skvs(skvs_dir)
+
+    if args.skv_to_bin:
+        start_time = time.time()
+        skv_to_bin(skvs_dir)
+        end_time = time.time()
+        print("skv_to_bin() took " + str(end_time - start_time) + " seconds to run")
+    
+    if args.bin_to_csv:
+        start_time = time.time()
+        bin_to_csv(skvs_dir)
+        end_time = time.time()
+        print("bin_to_csv() took " + str(end_time - start_time) + " seconds to run")
+
+    main_end_time = time.time()
+
+    print('Done!')
+
+    print(f"run.py took {main_end_time - main_start_time} seconds to run")
+
---
+++ src/bin_to_csv.py
@@ -0,0 +1,293 @@
+import os
+import numpy as np
+import numpy.typing as npt
+import time
+import cv2
+
+from .pose_module import PoseLandmarker
+
+
+class BinToCsv():
+    """
+    BinToCsv is a class that takes .bin files as input, runs them through the pose estimation pipeline,
+    and saves the output to a .csv file.
+    """
+    def __init__(self, input_dir: str, output_filename: str, image_width: int = 640, image_height: int = 480, visualize_Pose: bool = False):
+        """
+        Initialize BinToCsv object
+
+        Args:
+            input_dir (str): absolute path to directory containing .bin files
+            output_filename (str): name of output .csv file
+            visualize_Pose (bool): whether or not to visualize pose skeleton.
+        """
+
+        # Directory containing input .bin files
+        self.input_dir = input_dir
+
+        # Name of output .csv file
+        self.output_filename = output_filename
+
+        # Define image width and height
+        self.image_width = image_width
+        self.image_height = image_height
+
+        # Whether or not to visualize pose skeleton
+        self.visualize_Pose = visualize_Pose
+
+        return
+    
+    def _get_bin_files(self):
+        """
+        Get list of .bin files in input_dir
+
+        Returns:
+            bin_files (list): list of .bin files in input_dir
+        """
+
+        # Get list of .bin files in input_dir
+        filelist = []
+
+        for filename in os.listdir(self.input_dir):
+            if filename.endswith(".bin"):
+                # Remove the ".bin" suffix
+                filename = filename[:-4]
+                filelist.append(filename)
+
+        return filelist
+    
+    def _read_binary_file(self, filepath: str) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Read a binary file containing x, y, z coordinates, and confidence values.
+
+        Args:
+            filepath: The path to the binary file to be read.
+
+        Returns:
+            A tuple containing four NumPy arrays: x_all, y_all, z_all, and confidence_all.
+            - x_all: An (n,d) array of x-coordinates.
+            - y_all: An (n,d) array of y-coordinates.
+            - z_all: An (n,d) array of z-coordinates.
+            - confidence_all: An (n,d) array of confidence values.
+
+        This method reads a binary file and extracts x, y, z coordinates, and confidence values.
+        The binary file is assumed to have a specific structure where each array is stored sequentially.
+
+        Note: This method assumes that the binary file is properly formatted and contains valid data.
+
+        Example usage:
+            x, y, z, confidence = _read_binary_file('data.bin')
+        """
+        x_all, y_all, z_all, confidence_all = np.array([]), np.array([]), np.array([]), np.array([])
+
+        NUM_FRAMES_PER_FILE = 600
+
+        with open(filepath, 'rb') as binary_file:
+            x_all = np.frombuffer(binary_file.read(NUM_FRAMES_PER_FILE * 307200 * 2), dtype=np.int16).reshape((NUM_FRAMES_PER_FILE, 307200)).transpose()
+            y_all = np.frombuffer(binary_file.read(NUM_FRAMES_PER_FILE * 307200 * 2), dtype=np.int16).reshape((NUM_FRAMES_PER_FILE, 307200)).transpose()
+            z_all = np.frombuffer(binary_file.read(NUM_FRAMES_PER_FILE * 307200 * 2), dtype=np.int16).reshape((NUM_FRAMES_PER_FILE, 307200)).transpose()
+            confidence_all = np.frombuffer(binary_file.read(NUM_FRAMES_PER_FILE * 307200 * 2), dtype=np.int16).reshape((NUM_FRAMES_PER_FILE, 307200)).transpose()
+        
+        # Get number of frames (columns) in this video clip
+        # num_frames = np.size(gray_all, 1)
+        num_frames = np.shape(confidence_all)[1]
+
+        # Each array is currently (height*width, num_frames) = (480*640, num_frames) = (307200, num_frames)
+        # Reshape to (height, width, num_frames) = (480, 640, num_frames)
+        x_all = x_all.reshape([self.image_height, self.image_width, num_frames])
+        y_all = y_all.reshape([self.image_height, self.image_width, num_frames])
+        z_all = z_all.reshape([self.image_height, self.image_width, num_frames])
+        confidence_all = confidence_all.reshape([self.image_height, self.image_width, num_frames])
+
+        return x_all, y_all, z_all, confidence_all
+    
+    
+    def _convert_camera_confidence_to_grayscale(self, confidence_array: npt.NDArray[np.int16]) -> np.ndarray:
+        """
+        Convert the input confidence array to grayscale and scale down the brightness to help
+        with face detection.
+
+        Args:
+            confidence_array: An (n, d) confidence image in the format outputted by the IMX520 camera.
+
+        Returns:
+            An (n, d) grayscale image containing grayscale intensity values in the range [0, 255].
+        """
+
+        # brightness_scaling_factor = 4
+        
+        # grayscale_img = confidence_array.astype(float)
+        # grayscale_img = grayscale_img * brightness_scaling_factor
+        # grayscale_img[np.where(grayscale_img > 255)] = 255
+        # grayscale_img = grayscale_img.astype('uint8')
+
+        # return grayscale_img
+
+        # This is a new implementation that I believe should be more resilient to
+        # changes in the lighting conditions of the scene.
+
+        # Normalize the confidence values to the range [0, 1]
+        min_val = np.min(confidence_array)
+        max_val = np.max(confidence_array)
+        difference = max_val - min_val
+
+        if difference != 0:
+            normalized_data = (confidence_array - min_val) / difference
+
+            # Map the normalized data to the range [0, 255]
+            grayscale_image = (normalized_data * 255).astype(np.uint8)
+
+            return grayscale_image
+        else:
+            return np.zeros(confidence_array.shape, dtype=np.uint8)
+    
+
+    def _process_pose_landmarks(
+        self,
+        landmarks_pixels: np.ndarray,
+        frame_idx: int,
+        frame_x: np.ndarray,
+        frame_y: np.ndarray,
+        frame_z: np.ndarray,
+        frame_confidence: np.ndarray,
+        frame_grayscale_rgb: np.ndarray
+    ) -> None:
+        
+        print("Hi")
+
+        return
+    
+    
+    def _process_file(self, file_num: int, num_files_to_process: int, filename: str, pose_detector: PoseLandmarker) -> None:
+        """
+        Load and process .bin file
+
+        Args:
+            file_num (int): number of file being processed
+            num_files_to_process (int): total number of files to process
+            filename (str): name of file to process
+
+        Returns:
+            None
+        """
+
+        print(f"Processing file {file_num}/{num_files_to_process}: {filename}...")
+
+        # Load the file
+        filepath = os.path.join(self.input_dir, filename + '.bin')
+        x_all, y_all, z_all, confidence_all = self._read_binary_file(filepath)
+
+        # # Get number of frames (columns) in this video clip
+        # # num_frames = np.size(gray_all, 1)
+        # num_frames = np.shape(confidence_all)[1]
+
+        # # Each array is currently (height*width, num_frames) = (480*640, num_frames) = (307200, num_frames)
+        # # Reshape to (height, width, num_frames) = (480, 640, num_frames)
+        # x_all = x_all.reshape([self.image_height, self.image_width, num_frames])
+        # y_all = y_all.reshape([self.image_height, self.image_width, num_frames])
+        # z_all = z_all.reshape([self.image_height, self.image_width, num_frames])
+        # confidence_all = confidence_all.reshape([self.image_height, self.image_width, num_frames])
+
+        # Get number of frames in this video clip
+        num_frames = np.shape(confidence_all)[2]
+
+        # Used to calculate FPS
+        previous_time = 0
+        start_time = time.time()
+
+        # Loop through all frames
+        for frame_idx in range(num_frames):
+            frame_x = x_all[:, :, frame_idx]
+            frame_y = y_all[:, :, frame_idx]
+            frame_z = z_all[:, :, frame_idx]
+            frame_confidence = confidence_all[:, :, frame_idx]
+
+            # Track face and extract intensity and depth for all ROIs in this frame
+
+            # Convert the frame's confidence values to a grayscale image (n,d)
+            frame_grayscale = self._convert_camera_confidence_to_grayscale(frame_confidence)
+
+            # # To improve performance, optionally mark the image as not writeable to
+            # # pass by reference.
+            # frame_grayscale.flags.writeable = False
+
+            # Convert grayscale image to "RGB" (n,d,3)
+            frame_grayscale_rgb = cv2.cvtColor(frame_grayscale, cv2.COLOR_GRAY2RGB)
+
+            # Get pixel locations of all pose landmarks
+            # face_detected, landmarks_pixels = face_mesh_detector.find_face_mesh(image=frame_grayscale_rgb, draw=self.visualize_FaceMesh)
+            pose_detected, contains_invalid_landmarks, landmarks_pixels = pose_detector.get_landmarks(image=frame_grayscale_rgb, draw=self.visualize_Pose)
+
+            if pose_detected:
+                # multithreading_tasks.append(self.thread_pool.submit(self._process_face_landmarks, landmarks_pixels, frame_idx, frame_x, frame_y, frame_z, frame_confidence, intensity_signal_current_file, depth_signal_current_file, ear_signal_current_file, frame_grayscale_rgb))
+                self._process_pose_landmarks(landmarks_pixels, frame_idx, frame_x, frame_y, frame_z, frame_confidence, frame_grayscale_rgb)
+
+            if self.visualize_Pose:
+                # Calculate and overlay FPS
+
+                current_time = time.time()
+                # FPS = (# frames processed (1)) / (# seconds taken to process those frames)
+                fps = 1 / (current_time - previous_time)
+                previous_time = current_time
+                cv2.putText(frame_grayscale_rgb, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+                # TODO: Overlay frame number in top right corner
+                # cv2.putText(frame_grayscale_rgb, f'Frame {frame_idx + 1}', (20, 120), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+                text = f'{frame_idx + 1}'
+                text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 2)[0]
+                text_x = frame_grayscale_rgb.shape[1] - text_size[0] - 20  # Position text at the top right corner
+                text_y = text_size[1] + 20
+                cv2.putText(frame_grayscale_rgb, text, (text_x, text_y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+
+                # Display frame
+
+                cv2.imshow("Image", frame_grayscale_rgb)
+                cv2.waitKey(1)
+        
+        # Calculate and print average FPS
+        end_time = time.time()
+        average_fps = num_frames / (end_time - start_time)
+        print(f"Average FPS: {average_fps}")
+
+        return
+    
+    def run(self):
+        """
+        Run pose estimation pipeline on all .bin files.
+        """
+
+        # Get list of .bin files in input_dir
+        bin_files = self._get_bin_files()
+
+        # Load and process every input video file
+        file_num = 0
+        num_files_to_process = len(bin_files)
+
+        # Define MediaPipe detectors
+        pose_detector = PoseLandmarker(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+        for filename in bin_files:
+            file_num += 1
+
+            # Process the file
+            self._process_file(file_num, num_files_to_process, filename, pose_detector)
+
+        return
+
+
+def main():
+    skvs_dir = os.path.join(os.path.dirname(os.getcwd()), 'Data', 'skvs')
+
+    # Get absolute path of directory where .bin files are located
+    bins_dir = os.path.join(skvs_dir, "bins")
+
+    # Run pose estimation pipeline on all .bin files in bins_dir and save output to csvs_dir
+    myBinToCsv = BinToCsv(input_dir=bins_dir, output_filename="my_csv", visualize_Pose=True)
+    myBinToCsv.run()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
