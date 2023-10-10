14b49cb40387ea7307110f610ef599a6240800f2
grabad <george.a.rabadi@gmail.com>
2023-10-10 12:14:15 -0400

    removed extra files, reorganized

--- sub/recording/stopScript.m
+++
@@ -1,6 +0,0 @@
-% stopScript - stops script if figure was closed.
-
-function stopScript(object_handle, event)
-    global timesofrec;
-    timesofrec = -1; % exit while-loop that triggers data logging 
-end
