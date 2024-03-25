ae27076cefccb1977b0ef123840c9b5e5cbc7d55
grabad <george.a.rabadi@gmail.com>
2024-03-25 15:14:42 -0400

    added graph labels to frame plotter

--- plot_rgb_frame.py
+++ plot_rgb_frame.py
@@ -10,12 +10,16 @@ frame_num = 200
 
 plt.subplot(1,4,1)
 plt.imshow(xyz[:,:,0, frame_num], cmap="PiYG", norm=colors.TwoSlopeNorm(vcenter=0))
+plt.title('X')
 plt.subplot(1,4,2)
 plt.imshow(xyz[:,:,1, frame_num], cmap="PiYG", norm=colors.TwoSlopeNorm(vcenter=0))
+plt.title('Y')
 plt.subplot(1,4,3)
 plt.imshow(xyz[:,:,2, frame_num], cmap="PiYG", norm=colors.TwoSlopeNorm(vcenter=0))
+plt.title('Z')
 
 plt.subplot(1,4,4)
 plt.imshow(rgb[:, :, :, frame_num])
+plt.title('RGB')
 
 plt.show()
