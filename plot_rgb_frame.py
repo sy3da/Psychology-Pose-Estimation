f5332c60adc9aaa7091f7c41cd1b98a77bad8520
grabad <george.a.rabadi@gmail.com>
2024-03-26 15:49:25 -0400

    swapped lines in plot_rgb

--- plot_rgb_frame.py
+++ plot_rgb_frame.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-data = np.load('Data/npz/george_b1.npz')
+data = np.load('Data/npz/lauren_baseline_test.npz')
+frame_num = 100
+
 xyz = data['xyz_values']
 rgb = data['rgb_values']
 
-frame_num = 200
 
 plt.subplot(1,4,1)
 plt.imshow(xyz[:,:,0, frame_num], cmap="PiYG", norm=colors.TwoSlopeNorm(vcenter=0))
